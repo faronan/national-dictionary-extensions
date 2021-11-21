d7a270b49b380fce9abbe2171410d495d4629580
faronan <toshiki.ito88@gmail.com>
2021-11-21 21:01:28 +0900

    format by prettier

--- src/pages/popup.tsx
+++ src/pages/popup.tsx
@@ -1,29 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import type { NextPage } from 'next'
-import { changeButtonColor,changeText } from './lib/changeButtonColor'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import type { NextPage } from 'next';
+import { changeButtonColor, changeText } from './lib/changeButtonColor';
 
 const Popup: NextPage = () => {
   const buttonStyle = {
-    "height": "30px",
-    "width": "30px"
-  }
+    height: '30px',
+    width: '30px',
+  };
   return (
     <div>
-      <main style={{ minWidth: "700px" }}>
+      <main style={{ minWidth: '700px' }}>
         <button id="changeColor" style={buttonStyle}></button>
         <text id="text">hoge</text>
       </main>
     </div>
-  )
-}
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
     <Popup />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
-changeButtonColor()
-changeText()
+changeButtonColor();
+changeText();
