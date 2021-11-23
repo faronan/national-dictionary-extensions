0a85c019b46cf4049ec96f61fee17058f9169420
faronan <toshiki.ito88@gmail.com>
2021-11-24 00:28:01 +0900

    rm tutorial code

--- src/pages/background.ts
+++ src/pages/background.ts
@@ -1,7 +1,1 @@
 import React from 'react';
-const color = '#3aa757';
-
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
-});
