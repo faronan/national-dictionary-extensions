e7e28018013495cab1cb66f0d537cf4e7b629ba5
faronan <toshiki.ito88@gmail.com>
2021-11-25 00:30:06 +0900

    mv logic lib to popup

--- src/pages/popup.tsx
+++ src/pages/popup.tsx
@@ -1,25 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import type { NextPage } from 'next';
-import { main } from './lib/popupLib';
 
 const Popup: NextPage = () => {
+  const [storageValue, setStorageValue] = useState('');
+  const [input, setInput] = useState('');
+  const [result, setResult] = useState('');
+
   const buttonStyle = {
     height: '30px',
     width: '50px',
   };
+  const searchDictionary = async () => {
+    const url = `https://www.weblio.jp/content/${input}`;
+    const kijiElements = await fetch(url)
+      .then((response) => response.text())
+      .then((text) => new DOMParser().parseFromString(text, 'text/html'))
+      .then((document) => document.getElementsByClassName('kiji') as HTMLCollectionOf<HTMLElement>);
+
+    if (kijiElements.length != 0) {
+      const children = kijiElements[0].children as HTMLCollectionOf<HTMLElement>;
+      setResult(children[1].innerText);
+    } else {
+      setResult(`用語解説で「${input}」に一致する見出し語は見つかりませんでした。`);
+    }
+  };
+
+  chrome.storage.local.get('selectionString', ({ selectionString }) => {
+    setStorageValue(selectionString);
+  });
+  useEffect(() => {
+    setInput(storageValue);
+  }, [storageValue]);
+
   return (
-    <div>
-      <main style={{ minWidth: '700px' }}>
-        <div>
-          <input type="text" id="input"></input>
-          <button id="searchButton" style={buttonStyle}>
-            検索
-          </button>
-        </div>
-        <text id="searchResult"></text>
-      </main>
-    </div>
+    <main style={{ minWidth: '700px' }}>
+      <div>
+        <input type="text" value={input} onChange={(e) => setInput(e.target.value)}></input>
+        <button style={buttonStyle} onClick={searchDictionary}>
+          検索
+        </button>
+      </div>
+      <text>{result}</text>
+    </main>
   );
 };
 
@@ -29,5 +52,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-main();
